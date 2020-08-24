65a9f8426faf116ad8a780eb3067bab9104105a7
linda <linda@lindade-MacBook-Pro.local>
2020-08-24 14:39:55 +0800

    Homework

--- app.js
+++ app.js
@@ -1,94 +1,63 @@
-let express = require('express');
-let firebase = require('firebase')
+const express = require("express");
+const firebase = require("firebase");
+const Swal = require("sweetalert2");
 var firebaseConfig = {
-    apiKey: "AIzaSyB7FaqJO7ioGrBAw1pr6cFLjIkFEjLCjOc",
-    authDomain: "firstproject-d9cc3.firebaseapp.com",
-    databaseURL: "https://firstproject-d9cc3.firebaseio.com",
-    projectId: "firstproject-d9cc3",
-    storageBucket: "firstproject-d9cc3.appspot.com",
-    messagingSenderId: "808170250991",
-    appId: "1:808170250991:web:a08bdb4e3da6aa407cb11a",
-    measurementId: "G-P6JJVM8SD9"
+  apiKey: "AIzaSyB7FaqJO7ioGrBAw1pr6cFLjIkFEjLCjOc",
+  authDomain: "firstproject-d9cc3.firebaseapp.com",
+  databaseURL: "https://firstproject-d9cc3.firebaseio.com",
+  projectId: "firstproject-d9cc3",
+  storageBucket: "firstproject-d9cc3.appspot.com",
+  messagingSenderId: "808170250991",
+  appId: "1:808170250991:web:a08bdb4e3da6aa407cb11a",
+  measurementId: "G-P6JJVM8SD9",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-let db = firebase.firestore();
-let app = express();
-app.use(express.static('./public'));
-app.set('view engine', 'ejs');  
-app.get('/', async (req, res) => {  
-    // let data = await db.collection('classA').get();
-    // let userArr = []
-    // data.forEach((doc) => {
-    //     console.log(doc.data().name)
-    //     userArr.push(doc.data().name);
-    // })
-    res.render('default', {  
-        title: '首頁',  
-        users: ['Fisheep', 'Fiona', 'Alice', 'Bob']
-        // users: userArr
-    });  
+const db = firebase.firestore();
+const app = express();
+app.use(express.static("./public"));
+app.set("view engine", "ejs");
+
+app.get("/", async (req, res) => {
+  res.render("default");
 });
 
-app.get("/firebase-test", async (req, res) => {
-    let html = '';
-    let data = await db.collection('classA').get();
-    data.forEach(doc => {
-        console.log(doc.data());
-        html += `<div>${doc.id}: name = ${doc.data().name} age = ${doc.data().age}</div>`;
+// app.get("/login", (req, res) => {
+//   res.render("login.ejs");
+// });
+
+app.get("/member", async (req, res) => {
+  let data = await db.collection("classA").get();
+  let userArr = [];
+  data.forEach((doc) => {
+    userArr.push({
+      id: doc.id,
+      name: doc.data().name,
+      age: doc.data().age,
+      gender: doc.data().gender,
     });
-    res.send(html)
-})
-
-app.get("/classA_backend", async (req, res) => {
-    let data = await db.collection('classA').get();
-    userArr = []
-    data.forEach((doc) => {
-        userArr.push({
-            id: doc.id,
-            name: doc.data().name,
-            age: doc.data().age,
-            gender: doc.data().gender
-        })
-    })
-    res.render('classA', {
-        users: userArr
-    })
-})
-
-app.get("/classA_frontend", (req, res) => {
-    let options = {
-        root:  __dirname+"/public",
-        dotfiles: 'ignore'
-    }
-    console.log(__dirname+"/public");
-    res.sendFile("/classA.html", options);
-})
-
-app.get('/who/:name', (req, res) => {  
-    var name = req.params.name;  
-    res.send(`This is ${name}`);  
+  });
+  res.render("member", {
+    users: userArr,
+  });
 });
 
-app.get('/API/deleteMember', (req, res) => {
-    db.collection('classA').doc(req.query.id).delete();
-    console.log(req.query.id);
-    res.send(`delete Member id = ${req.query.id}!`)
-})
-
-app.get('/API/addMember', (req, res) => {
-    db.collection('classA').add({
-        name: req.query.name,
-        gender: req.query.age,
-        age: req.query.gender
-    });
-    console.log("Add member !!");
-    res.send("Add member success!");
-})
-
-app.get('*', (req, res) => {  
-    res.send('No Content');  
+app.get("/insert", (req, res) => {
+  res.render("insert");
+  db.collection("classA").add({
+    name: req.query.name,
+    age: req.query.age,
+    gender: req.query.gender,
+  });
+  console.log("Insert successful");
 });
+
+app.get("/API/deleteMember", (req, res) => {
+  db.collection("classA").doc(req.query.id).delete();
+  console.log(req.query.id);
+  res.send(`delete Member id = ${req.query.id}!`);
+});
+
 let port = process.env.PORT || 3000;
-app.listen(port); 
+app.listen(port);
 console.log("server running at port = ", port);
--- public/backend_setEvent.js
+++ public/backend_setEvent.js
@@ -1,30 +1,20 @@
-const cross = document.querySelectorAll('#cross-family')
-const form = document.querySelector("#add-students-form");
-for(let i = 0 ; i < cross.length; i ++){
-    cross[i].addEventListener('click', async (test) => {
-        test.stopPropagation();
-        let id = cross[i].getAttribute('cross-id');
-        console.log(id);
-        let apiUrl = `http://127.0.0.1:3000/API/deleteMember?id=${id}`
-        let res = await fetch(apiUrl, {method:'GET'});
-        let text = await res.text();
-        console.log(text);
-        console.log("in the cross, id = ", id);
-        // call delete api
-    });
-}
-// add data
-form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    // call add api
-    name    = form.name.value,
-    gender  = form.gender.value,
-    age     = form.age.value
-    let apiUrl = `http://127.0.0.1:3000/API/addMember?name=${name}&gender=${gender}&age=${age}`
-    let res = await fetch(apiUrl, {method:'GET'});
+const cross = document.querySelectorAll("#cross-family");
+for (let i = 0; i < cross.length; i++) {
+  cross[i].addEventListener("click", async (test) => {
+    test.stopPropagation();
+    let id = cross[i].getAttribute("cross-id");
+    console.log(id);
+    let apiUrl = `https://git.heroku.com/practiceproject001.git/API/deleteMember?id=${id}`;
+    let res = await fetch(apiUrl, { method: "GET" });
     let text = await res.text();
     console.log(text);
-    form.name.value = '';
-    form.gender.value = '';
-    form.age.value = '';
-});
+    console.log("in the cross, id = ", id);
+    // call delete api
+  });
+}
+function delForm() {
+  if (confirm("Do you really want to delete this member?")) {
+    alert("Delete confirm");
+    window.location.reload();
+  }
+}
